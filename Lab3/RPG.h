d7bd2fd0c36612fcaf3a76279ff432030d69c43e
EverestOw <54610621+EverestOw@users.noreply.github.com>
2026-09-17 18:04:24 +0000

    Forward declared RPG class and member functions

--- Lab3/RPG.h
+++ Lab3/RPG.h
@@ -1,1 +1,22 @@
+#ifndef RPG_H
+#define RPG_H
 
+#include <string>
+
+//  RPG Class
+class RPG {
+    public:
+    // Getter Functions
+    std::string getName();
+    int getHealth();
+    int getStrength();
+    int getDefense();
+
+    // Setter Functions
+    void setSkills();
+
+    // Utility functions
+    void printAction();
+};
+
+#endif
