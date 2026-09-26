35d571deaad4ea8f96296adc27e1efb785f589be
Everest Ow <everestow@outlook.com>
2026-09-25 23:17:32 -0700

    expanded output for unit tests

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -3,12 +3,13 @@
 
 void printRPGobject(const RPG &RPG_object) {
     printf(
-        "definedPerson:\n"
+        "RPG Object at 0x%X\n"
         "\tName:\t %s\n"
         "\tHits Taken:\t %i\n"
         "\tLuck:\t %f\n"
         "\tExp:\t %f\n"
         "\tLevel:\t %i\n",
+        &RPG_object,
         RPG_object.getName().c_str(),
         RPG_object.getHitsTaken(),
         RPG_object.getLuck(),
@@ -18,24 +19,52 @@ void printRPGobject(const RPG &RPG_object) {
 }
 
 int main() {
-    RPG defaultPerson = RPG();
-    RPG definedPerson = RPG("Named one", 1, 2.2, 3.3, 4);
 
     // **Testing constructors, accessors
-    // printRPGobject(defaultPerson);
-    // std::cout << std::endl;
-    // printRPGobject(definedPerson);
+    printf("Testing constructors\n");
+    RPG defaultPerson = RPG();
+    printRPGobject(defaultPerson);
+
+    std::cout << std::endl;
+
+    RPG definedPerson = RPG("Named one", 1, 2.2, 3.3, 4);
+    printRPGobject(definedPerson);
+
+    std::cout << std::endl;
 
     // **Testing Mutators**
-    printf("Alive? :\t %b\n",definedPerson.isAlive());
-    definedPerson.setHitsTaken(-1);
-    printf("Dead?:\t %b\n", definedPerson.isAlive());
+    // Event: test dummy takes 1 damage
+    printf(
+        "Testing Mutators:\n"
+        "\t%s failed their constitution roll. %s takes 1 damage!\n",
+        definedPerson.getName().c_str(),
+        definedPerson.getName().c_str()
+        );
+    definedPerson.setHitsTaken(definedPerson.getHitsTaken() + 1);
 
-    definedPerson.setHitsTaken(3);
-    printf("Dead?:\t %b\n", definedPerson.isAlive());
+    printf(
+        "\t%s is %s with %i hits taken.\n",
+        definedPerson.getName().c_str(),
+        definedPerson.isAlive() ? "alive" : "done for",
+        definedPerson.getHitsTaken()
+    );
 
-    definedPerson.setHitsTaken(4);
-    printf("Dead?:\t %b\n", definedPerson.isAlive());
+    std::cout << std::endl;
+
+    // Event: test dummy takes 2 damage
+    printf("\t%s slipped! %s takes 2 damage!\n",
+        definedPerson.getName().c_str(),
+        definedPerson.getName().c_str()
+    );
+
+    definedPerson.setHitsTaken(definedPerson.getHitsTaken() + 2);
+
+    printf(
+        "\t%s is %s with %i hits taken.\n",
+        definedPerson.getName().c_str(),
+        definedPerson.isAlive() ? "alive" : "a goner",
+        definedPerson.getHitsTaken()
+    );
 
     return 0;
 }
